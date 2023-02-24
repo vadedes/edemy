fd130469f339d0b237f697d51ec379fe238a850c
Dave <87390532+vadedes@users.noreply.github.com>
2023-02-24 23:37:35 +0800

    Added create course form component with relevant props

---
+++ client/components/forms/CourseCreateForm.js
@@ -0,0 +1,97 @@
+import { Select, Button, Avatar, Badge } from "antd";
+
+const { Option } = Select;
+
+const CourseCreateForm = ({ handleSubmit, handleImage, handleChange, values, setValues }) => {
+  const children = [];
+  for (let i = 9.99; i <= 100.99; i++) {
+    children.push(<Option key={i.toFixed(2)}>${i.toFixed(2)}</Option>);
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className='flex flex-col justify-center items-center w-full'>
+        <div className='w-5/6'>
+          <input
+            type='text'
+            name='name'
+            className='mb-4 p-4 border-solid border-2 border-slate-300 rounded-md w-full'
+            placeholder='Name'
+            value={values.name}
+            onChange={handleChange}
+          />
+        </div>
+        <div className='w-5/6'>
+          <textarea
+            name='description'
+            placeholder='Write course description'
+            className='mb-4 p-4 border-solid border-2 border-slate-300 rounded-md w-full'
+            cols='7'
+            rows='7'
+            value={values.description}
+            onChange={handleChange}></textarea>
+        </div>
+        <div className='w-5/6 flex flex-col'>
+          <div className='w-full pr-2'>
+            <Select
+              style={{ width: "100%" }}
+              size='large'
+              value={values.paid}
+              onChange={(v) => setValues({ ...values, paid: v, price: 0 })}>
+              <Option value={true}>Paid</Option>
+              <Option value={false}>Free</Option>
+            </Select>
+            {values.paid && (
+              <div className='w-full pt-4'>
+                <Select
+                  defaultValue='$9.99'
+                  style={{ width: "100%" }}
+                  onChange={(v) => setValues({ ...values, price: v })}
+                  tokenSeparators={[,]}
+                  size='large'>
+                  {children}
+                </Select>
+              </div>
+            )}
+            <div className='w-full pt-4'>
+              <input
+                type='text'
+                name='category'
+                className='mb-4 p-4 border-solid border-2 border-slate-300 rounded-md w-full'
+                placeholder='Category'
+                value={values.category}
+                onChange={handleChange}
+              />
+            </div>
+            <div className='w-full mt-4 mb-4 '>
+              <label className='block w-full mb-4 p-3 border-solid border-2 border-slate-300 rounded-md cursor-pointer font-bold text-slate-800 hover:bg-slate-400 hover:text-white transition ease-in-out duration-300'>
+                {values.loading ? "Uploading" : "Image upload"}
+                <input
+                  type='file'
+                  name='image'
+                  onChange={handleImage}
+                  accept='image/*'
+                  hidden
+                  className='w-full'
+                />
+              </label>
+            </div>
+          </div>
+          <div className='w-full mt-4'>
+            <Button
+              className='px-6 py-4 h-[55px] flex justify-center items-center uppercase font-bold w-full bg-slate-500 text-white rounded-md transition ease-in-out duration-300 hover:!text-white '
+              onClick={handleSubmit}
+              disabled={values.loading || values.uploading}
+              loading={values.loading}
+              type='primary'>
+              {values.loading ? "Saving..." : "Save & Continue"}
+            </Button>
+          </div>
+        </div>
+      </form>
+      ;
+    </>
+  );
+};
+
+export default CourseCreateForm;
--- client/pages/instructor/course/create.js
+++ client/pages/instructor/course/create.js
@@ -1,9 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import InstructorRoute from "@/components/routes/InstructorRoute";
-import { Select, Button, Avatar, Badge } from "antd";
-
-const { Option } = Select;
+import CourseCreateForm from "@/components/forms/CourseCreateForm";
 
 const Coursecreate = () => {
   const [values, setValues] = useState({
@@ -12,6 +10,7 @@ const Coursecreate = () => {
     price: "9.99",
     uploading: false,
     paid: true,
+    category: "",
     loading: false,
     imagePreview: "",
   });
@@ -29,77 +28,22 @@ const Coursecreate = () => {
     console.log(values);
   };
 
-  const children = [];
-  for (let i = 9.99; i <= 100.99; i++) {
-    children.push(<Option key={i.toFixed(2)}>${i.toFixed(2)}</Option>);
-  }
-
-  const courseCreateForm = () => (
-    <form onSubmit={handleSubmit} className='flex flex-col justify-center items-center w-full'>
-      <div className='w-5/6'>
-        <input
-          type='text'
-          name='name'
-          className='mb-4 p-4 border-solid border-2 border-slate-300 rounded-md w-full'
-          placeholder='Name'
-          value={values.name}
-          onChange={handleChange}
-        />
-      </div>
-      <div className='w-5/6'>
-        <textarea
-          name='description'
-          placeholder='Write course description'
-          className='mb-4 p-4 border-solid border-2 border-slate-300 rounded-md w-full'
-          cols='7'
-          rows='7'
-          value={values.description}
-          onChange={handleChange}></textarea>
-      </div>
-      <div className='w-5/6 flex'>
-        <div className='w-3/6 pr-2'>
-          <Select
-            style={{ width: "100%" }}
-            size='large'
-            value={values.paid}
-            onChange={(v) => setValues({ ...values, paid: v, price: 0 })}>
-            <Option value={true}>Paid</Option>
-            <Option value={false}>Free</Option>
-          </Select>
-          {values.paid && (
-            <div className='w-full pt-4'>
-              <Select
-                defaultValue='$9.99'
-                style={{ width: "100%" }}
-                onChange={(v) => setValues({ ...values, price: v })}
-                tokenSeparators={[,]}
-                size='large'>
-                {children}
-              </Select>
-            </div>
-          )}
-          <div className='w-full pt-4'>
-            <input
-              type='text'
-              name='category'
-              className='mb-4 p-4 border-solid border-2 border-slate-300 rounded-md w-full'
-              placeholder='Category'
-              value={values.category}
-              onChange={handleChange}
-            />
-          </div>
-        </div>
-        <div className='w-3/6 pl-2'>Upload button here</div>
-      </div>
-    </form>
-  );
-
   return (
     <>
       <InstructorRoute className='container mx-auto py-10 flex flex-col justify-center items-center w-96'>
         <h1 className='text-5xl text-bold text-center mx-auto mb-10'>Create Course</h1>
 
-        <div className='pt-3 pb-3'>{courseCreateForm()}</div>
+        <div className='pt-3 pb-3'>
+          <CourseCreateForm
+            handleSubmit={handleSubmit}
+            handleChange={handleChange}
+            handleImage={handleImage}
+            values={values}
+            setValues={setValues}
+          />
+        </div>
+        <pre>{JSON.stringify(values, null, 4)}</pre>
+        {/* preview data in json format in the frontend for easy viewing */}
       </InstructorRoute>
     </>
   );
